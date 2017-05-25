615cd597ac06f043d20280b5f03ba7dcc22924a8
olive <olive@meishakeji.com>
2017-05-25 16:01:55 +0800

    t

---
+++ src/components/swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- Slider main container -->
+  <div class="swiper-container">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slides -->
+      <!--<div class="swiper-slide" v-for="src in imgArr"><img :src="src"></div>-->
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+
+<script>
+  import 'swiper/dist/css/swiper.css'
+  import Swiper from 'swiper'
+  export default {
+    data(){
+      return {
+        imgArr: [
+          'http://meishakeji-oss1.img-cn-shenzhen.aliyuncs.com/community/coursepic/3c20b82099594d6a9d36e05c08ba8868.jpeg@80p'
+          , 'http://meishakeji-oss1.img-cn-shenzhen.aliyuncs.com/community/coursepic/82e68c96d1bb4a04951fa29bb54ac3f5.png@50q']
+      }
+    },
+    created(){
+      setTimeout(() => {
+          console.log(1)
+        new Swiper('.swiper-container', {
+          direction: 'horizontal',
+          loop: true,
+          autoplay:true,
+          pagination: '.swiper-pagination',
+        })
+      }, 500)
+    }
+  }
+</script>
+<style lang="scss" rel="stylesheet/scss">
+
+  .swiper-container {
+    width: 600px;
+    height: 300px;
+    img {
+      max-width: 100%;
+    }
+  }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="page">
-    这是index
-    <router-link :to="{path:'/page1'}">page1</router-link>
+     <swiper></swiper>
 
-    <div style="margin-top: 50px"></div>
-    <div style="height: 35vh;background-color: red;"></div>
+<!--    这是index
+    <router-link :to="{path:'/page1'}">page1</router-link>-->
+
+    <div style="margin-top: 50px"></div><!---->
+<!--    <div style="height: 35vh;background-color: red;"></div>
     <div style="height: 35vh;background-color: green;"></div>
-    <div style="height: 35vh;background-color: blue;"></div>
+    <div style="height: 35vh;background-color: blue;"></div>-->
   </div>
 </template>
 
 <script>
+  import swiper from '../components/swiper.vue'
   export default {
     name: 'app',
-    components: {},
+    components: {
+      swiper
+    },
     created () {
       document.title = '首页'
     }
